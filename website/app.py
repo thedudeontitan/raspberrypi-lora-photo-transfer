9346e9daa72373f06f11c4cdf718daaa143c338a
Ankit Kokane <97180942+thedudeontitan@users.noreply.github.com>
2023-06-27 19:58:19 +0530

    added login

--- website/app.py
+++ website/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, request, render_template, send_file, jsonify
+from flask import Flask, request, render_template, send_file, jsonify,  redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
+from werkzeug.security import check_password_hash
 import datetime
 import os
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///logs1.db'  # SQLite database for logs1
 app.config['SQLALCHEMY_BINDS'] = {
     'logs2': 'sqlite:///logs2.db'  # SQLite database for logs2
@@ -89,7 +94,52 @@ def handle_logs2():
 
         return jsonify({'logs': logs_list})
 
+class User:
+    def __init__(self, username):
+        self.username = username
+
+    def get_id(self):
+        return self.username
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+# Set up the login manager to load the user
+@login_manager.user_loader
+def load_user(username):
+    # Replace this with your logic to load the user from your data store
+    if username == 'admin':
+        return User(username)
+    return None
+
+# Login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Validate the username and password
+        if username == 'admin' and password == 'admin':
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            error = 'Invalid username or password'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
 
@@ -101,6 +151,7 @@ class Image:
         self.modified = modified
 
 @app.route('/gallery')
+@login_required
 def gallery():
     images = []
     folder_path = '/home/sirius/Work/photo_compression/website/static/LoRaComm_Img/'  # Update with your image folder path
@@ -127,6 +178,7 @@ def get_image(image_path):
     return send_file(file_path)
 
 @app.route('/gallery2')
+@login_required
 def gallery2():
     images = []
     folder_path = '/home/sirius/Work/photo_compression/website/static/LoRaComm_Img/'  # Update with your image folder path
